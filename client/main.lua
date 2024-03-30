8d6c919e9da6f9855c5826142f74b605843980a3
“DieserGhost” <james.kinzer@t-online.de>
2024-03-30 14:25:18 +0100

    fixed job nil field

--- client/main.lua
+++ client/main.lua
@@ -1,11 +1,21 @@
+local ESX = nil
+
 local ESX = exports['es_extended']:getSharedObject()
 
 Citizen.CreateThread(function()
+    while ESX == nil do
+        Citizen.Wait(10)
+    end
+
     while true do 
-        local PlayerESXName = ESX.PlayerData.name
-        local PlayerESXJob = ESX.PlayerData.job.name
+        local PlayerESXName = GetPlayerName(PlayerId())
+        local PlayerESXJob = ""
+        if ESX.PlayerData.job then
+            PlayerESXJob = ESX.PlayerData.job.name
+        else
+            PlayerESXJob = "No Job"
+        end
         local playerId = GetPlayerServerId(PlayerId())
-        local playerGroup = GetPlayerGroup(PlayerId()) 
         
         local appId = YOUR_DISCORD_APP_ID -- Deine Discord-App-ID hier einfügen
         local smallPic = 'small'
@@ -14,7 +24,7 @@ Citizen.CreateThread(function()
         SetDiscordAppId(appId)
 
         -- Discord Rich Presence aktualisieren
-        SetRichPresence("Name: " .. PlayerESXName .. "\nJob: " .. PlayerESXJob .. "\nID: " .. playerId .. "\nGroup: " .. playerGroup)
+        SetRichPresence("Name: " .. PlayerESXName .. "\nJob: " .. PlayerESXJob .. "\nID: " .. playerId)
         SetDiscordRichPresenceAsset(bigPic)
         SetDiscordRichPresenceAssetText('UltraCity')
         SetDiscordRichPresenceAssetSmall(smallPic)
@@ -23,3 +33,5 @@ Citizen.CreateThread(function()
         Citizen.Wait(1000) 
     end
 end)
+
+
